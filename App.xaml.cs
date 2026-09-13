7290c02da713b6343389ff2b0a1f9013aefc119c
yakov <d.yakowkin@yandex.ru>
2026-09-13 18:12:38 +0500

    feat: первичная настройка DI и создание MainWindow с MainVM

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using ThornMessenger.Presentation.ViewModels;
+using ThornMessenger.Presentation.Views;
 
 namespace ThornMessenger
 {
@@ -9,6 +12,29 @@ namespace ThornMessenger
     /// </summary>
     public partial class App : Application
     {
+        private IServiceProvider _serviceProvider;
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            var services = new ServiceCollection();
+
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+
+            var mainWindow = _serviceProvider.GetRequiredService<MainWindow>();
+            mainWindow.Show();
+
+        }
+
+        private void ConfigureServices(IServiceCollection services)
+        {
+            //ViewModels
+            services.AddSingleton<MainViewModel>();
+
+            //Views
+            services.AddSingleton<MainWindow>();
+        }
     }
 
 }
---
+++ Presentation/ViewModels/MainViewModel.cs
@@ -0,0 +1,15 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ThornMessenger.Presentation.ViewModels
+{
+    public class MainViewModel : ObservableObject
+    {
+        public MainViewModel()
+        {
+
+        }
+    }
+}
